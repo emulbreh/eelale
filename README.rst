edd39c0bcf28acb45a8e2ad00d9b6794732dc75f
Johannes Dollinger <emulbreh@googlemail.com>
2017-05-14 09:07:18 +0200

    Remove provisioning, fix autowheel call

--- README.rst
+++ README.rst
@@ -2,3 +2,10 @@ eelale
 ======
 
 cross-compiles Python wheels
+
+
+.. code-block:: console
+
+    $ eelale build --policy manylinux1_x86_64 --force-build :all: gevent
+
+    $ eelale build --image build-dependency-image psycopg2
--- eelale/cli.py
+++ eelale/cli.py
@@ -5,6 +5,10 @@ import hashlib
 import os
 import textwrap
 import shutil
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 def flatten(seqs):
@@ -12,20 +16,21 @@ def flatten(seqs):
 
 
 class Builder:
-    def __init__(self, base_image='ubuntu:xenial', script=None, python='python3', wheel_dir=None):
+    def __init__(self, base_image='ubuntu:xenial', python='python3', wheel_dir=None, policy=None):
         self.base_image = base_image
         self.build_dir = os.path.abspath('.eelale')
-        self.wheel_dir = wheel_dir or wheel_dir
+        self.wheel_dir = os.path.abspath(wheel_dir) if wheel_dir else self.build_dir
         self.pip_version = None
         self.python = python
-        self.script = script
+        self.policy = policy
 
     def vars(self):
         return {
             'baseimage': self.base_image,
             'python': self.python,
             'pip_version': '==%s' % self.pip_version if self.pip_version else '',
-            'script': os.path.abspath(self.script) if self.script else None,
+            'setuptools_version': '',
+            'auditwheel_version': '',
         }
 
     @property
@@ -33,45 +38,40 @@ class Builder:
         return textwrap.dedent("""
             FROM {baseimage}
             VOLUME /eelale/wheels
-            COPY provision.sh /eelale
-            RUN /bin/bash /eelale/provision.sh
             RUN {python} -m pip install -U pip{pip_version}
-            RUN {python} -m pip install auditwheel
+            RUN {python} -m pip install setuptools{setuptools_version}
+            RUN {python} -m pip install auditwheel{auditwheel_version}
         """).format(**self.vars()).strip()
 
     @property
     def image_name(self):
-        h = hashlib.sha256(self.dockerfile.encode('utf-8'))
-        if self.script:
-            with open(self.script, 'rb') as f:
-                h.update(f.read())
-        return 'eelale_%s' % h.hexdigest()[:10]
+        return 'eelale_%s' % hashlib.sha256(self.dockerfile.encode('utf-8')).hexdigest()[:10]
 
     def create_image(self):
         os.makedirs(self.build_dir, exist_ok=True)
         with open(os.path.join(self.build_dir, 'Dockerfile'), 'w') as f:
             f.write(self.dockerfile)
-        if self.script:
-            shutil.copy(self.script, os.path.join(self.build_dir, 'provision.sh'))
         image_name = self.image_name
         subprocess.run([
-            'docker', 'build', '--no-cache', '-t', image_name, self.build_dir
+            'docker', 'build', '-t', image_name, self.build_dir
         ])
 
     def run(self, cmd):
         self.create_image()
-        subprocess.run([
+        full_command = [
             'docker',
             'run',
             '--rm',
             '--volume', '%s:/eelale/wheels' % self.wheel_dir,
             self.image_name,
             *cmd
-        ])
+        ]
+        logger.info(full_command)
+        return subprocess.run(full_command, check=True)
 
     def build(self, packages, force=(':none:',)):
         self.create_image()
-        self.run([
+        wheel_output = self.run([
             self.python,
             '-m', 'pip',
             'wheel',
@@ -80,48 +80,41 @@ class Builder:
             *flatten(('--no-binary', package) for package in force),
             *packages
         ])
-        self.run([
-            self.python,
-            '-m', 'auditwheel',
-            'repair', '--help',
-        ])
-        for name in os.listdir(self.build_dir):
-            if name.endswith('.whl'):
-                self.run([
-                    self.python,
-                    '-m', 'auditwheel',
-                    'repair',
-                    '--wheel-dir', '/eelale/wheels',
-                    '/eelale/wheels/%s' % name,
-                ])
-                break
+        if self.policy:
+            for name in os.listdir(self.wheel_dir):
+                if name.endswith('.whl'):
+                    self.run([
+                        self.python,
+                        '-m', 'auditwheel',
+                        'repair',
+                        '--wheel-dir', '/eelale/wheels',
+                        '--plat', self.policy,
+                        '/eelale/wheels/%s' % name,
+                    ])
 
 
 @click.group()
 def main():
-    pass
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(name)s: %(message)s')
 
 
 @main.command()
 @click.option('--requirement', '-r', multiple=True)
-@click.option('--wheeldir', '-w', type=click.Path(), default='.')
+@click.option('--wheeldir', '-w', type=click.Path())
 @click.option('--image', default='quay.io/pypa/manylinux1_x86_64')
 @click.option('--python', default='/opt/python/cp36-cp36m/bin/python')
-@click.option('--prepare')
-@click.option('--manylinux1')
+@click.option('--policy')
 @click.option('--force-build', '-f', multiple=True)
 @click.argument('package', nargs=-1)
-def build(requirement, wheeldir, image, python, prepare, manylinux1, force_build, package):
+def build(requirement, wheeldir, image, python, policy, force_build, package):
     builder = Builder(
         base_image=image,
         python=python,
         wheel_dir=wheeldir,
-        script=prepare,
+        policy=policy,
     )
     print(builder.dockerfile)
-    builder.build(package, force=force_build)
-
-@main.command()
-def repair():
-    builder = Builder()
-    builder.repair()
+    try:
+        builder.build(package, force=force_build)
+    except subprocess.CalledProcessError as e:
+        raise click.ClickException(str(e)) from e
