452207674c66507c527caf16b252199a0908296f
Johannes Dollinger <emulbreh@googlemail.com>
2017-05-15 22:37:10 +0200

    Make --wheel-dir work

--- eelale/cli.py
+++ eelale/cli.py
@@ -15,23 +15,22 @@ def flatten(seqs):
 
 class Builder:
 
-    def __init__(self, base_image, python='python', build_dir='.eelale', wheel_dir=None, policy=None):
+    def __init__(self, base_image, python='python', build_dir='.eelale-build', wheel_dir=None, policy=None):
         self.base_image = base_image
         self.python = python
         self.build_dir = os.path.abspath(build_dir)
-        if wheel_dir:
-            # FIXME: arbitrary wheel dirs don't really work yet
-            self.wheel_dir = os.path.abspath(wheel_dir)
-        else:
-            self.wheel_dir = os.path.join(self.build_dir, 'wheels')
         self.policy = policy
         self.build_deps = ['pip', 'setuptools', 'auditwheel']
+
+    @property
+    def wheel_dir(self):
+        return os.path.join(self.build_dir, 'wheels')
 
     @property
     def dockerfile(self):
         lines = [
             'FROM %s' % self.base_image,
-            'VOLUME /eelale/volume',
+            'VOLUME /eelale',
         ]
         for dep in self.build_deps:
             lines.append('RUN %s -m pip install -U %s' % (self.python, dep))
@@ -42,7 +41,11 @@ class Builder:
         return 'eelale_%s' % hashlib.sha256(self.dockerfile.encode('utf-8')).hexdigest()[:10]
 
     def create_image(self):
+        if os.path.exists(self.build_dir):
+            logger.info('cleaning build dir')
+            shutil.rmtree(self.build_dir)
         os.makedirs(self.build_dir, exist_ok=True)
+        os.mkdir(self.wheel_dir)
         with open(os.path.join(self.build_dir, 'Dockerfile'), 'w') as f:
             f.write(self.dockerfile)
         image_name = self.image_name
@@ -51,12 +54,11 @@ class Builder:
         ])
 
     def run(self, cmd):
-        self.create_image()
         full_command = [
             'docker',
             'run',
             '--rm',
-            '--volume', '%s:%s' % (self.build_dir, '/eelale/volume'),
+            '--volume', '%s:%s' % (self.build_dir, '/eelale'),
             self.image_name,
             *cmd
         ]
@@ -67,31 +69,34 @@ class Builder:
         base, ext = os.path.splitext(path)
         filename = '%s%s' % (hashlib.sha1(base.encode('utf-8')).hexdigest(), ext)
         shutil.copy(path, os.path.join(self.build_dir, filename))
-        return '/eelale/volume/%s' % filename
+        return '/eelale/%s' % filename
 
     def build(self, *args, force=(':none:',)):
         self.create_image()
-        os.makedirs(self.wheel_dir)
         self.run([
             self.python,
             '-m', 'pip',
             'wheel',
-            '--wheel-dir', '/eelale/volume/wheels',
+            '--wheel-dir', '/eelale/wheels',
             '--no-deps',
             *flatten(('--no-binary', package) for package in force),
             *args
         ])
-        if self.policy:
-            for name in os.listdir(self.wheel_dir):
-                if name.endswith('.whl'):
-                    self.run([
-                        self.python,
-                        '-m', 'auditwheel',
-                        'repair',
-                        '--wheel-dir', '/eelale/volume/wheels',
-                        '--plat', self.policy,
-                        '/eelale/wheels/%s' % name,
-                    ])
+        paths = []
+        for name in os.listdir(self.wheel_dir):
+            if not name.endswith('.whl'):
+                continue
+            if self.policy:
+                self.run([
+                    self.python,
+                    '-m', 'auditwheel',
+                    'repair',
+                    '--wheel-dir', '/eelale/wheels',
+                    '--plat', self.policy,
+                    '/eelale/wheels/%s' % name,
+                ])
+            paths.append(os.path.join(self.wheel_dir, name))
+        return paths
 
 
 @click.group()
@@ -100,8 +105,8 @@ def main():
 
 
 @main.command()
-@click.option('--requirement', '-r', multiple=True)
-@click.option('--wheeldir', '-w', type=click.Path())
+@click.option('--requirement', '-r', multiple=True, help='Install from the given requirements file. This option can be used multiple times.')
+@click.option('--wheeldir', '-w', type=click.Path(), metavar='<dir>', help='Build wheels into <dir>, where the default is the current working directory.')
 @click.option('--image', default='quay.io/pypa/manylinux1_x86_64')
 @click.option('--python', default='/opt/python/cp36-cp36m/bin/python')
 @click.option('--policy')
@@ -111,10 +116,8 @@ def build(requirement, wheeldir, image, python, policy, force_build, package):
     builder = Builder(
         base_image=image,
         python=python,
-        wheel_dir=wheeldir,
         policy=policy,
     )
-    print(builder.dockerfile)
 
     def build_args():
         yield from package
@@ -123,6 +126,10 @@ def build(requirement, wheeldir, image, python, policy, force_build, package):
             yield builder.copy(req)
 
     try:
-        builder.build(*build_args(), force=force_build)
+        wheel_paths = builder.build(*build_args(), force=force_build)
     except subprocess.CalledProcessError as e:
         raise click.ClickException(str(e)) from e
+
+    os.makedirs(wheeldir, exist_ok=True)
+    for wheel_path in wheel_paths:
+        os.rename(wheel_path, os.path.join(wheeldir, os.path.basename(wheel_path)))
